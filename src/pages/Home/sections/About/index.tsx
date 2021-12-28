13c07aad9f389dc9ba3933deb32c46e47b565e1c
sohammondal <soham.pf.mondal@gmail.com>
2021-12-28 21:38:36 +0530

    feat(home/about): setup structure

--- src/pages/Home/sections/About/index.tsx
+++ src/pages/Home/sections/About/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import styled from 'styled-components'
 
-import { flexRowCenter } from 'theme'
-
-const Container = styled.section`
-    width: 100vw;
-    height: 60vh;
-    ${flexRowCenter}
-`
+import { Container, Section } from './styles'
 
 export const About: React.FC = () => {
-    return <Container>About</Container>
+    return (
+        <Container>
+            <Section>
+                <h1>Est 2021</h1>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industrys
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged.
+                </p>
+            </Section>
+            <Section>
+                <img
+                    src="http://canopusglobal.com/wp-content/uploads/2016/08/service-3.jpg"
+                    alt="all-products"
+                />
+            </Section>
+        </Container>
+    )
 }
