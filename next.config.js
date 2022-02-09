f13c22ee5033ccbd85a23f31cc05e1b4b42b33a2
sohammondal <soham.pf.mondal@gmail.com>
2022-02-09 22:48:49 +0530

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/* eslint-disable no-undef */
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    webpack5: false,
     env: {
         ENV: process.env.ENV,
         HOST: process.env.HOST,
     },
 }
+
+module.exports = nextConfig
